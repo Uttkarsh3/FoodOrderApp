6f2737ef726ee34508e8c790f0645fc53e2c69b1
uttkarsh <uttkarshbisht3@gmail.com>
2024-08-06 13:50:21 +0530

    Start Here

--- app/src/main/java/com/example/wavesoffood/Fragment/SearchFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragment/SearchFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wavesoffood.R
 import com.example.wavesoffood.adapter.MenuAdapter
 import com.example.wavesoffood.databinding.FragmentCartBinding
 import com.example.wavesoffood.databinding.FragmentSearchBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 
@@ -37,20 +39,65 @@ class SearchFragment : Fragment() {
 
     }
 
+
+private val filteredMenuFoodName= mutableListOf<String>()
+private val filteredMenuItemPrice=mutableListOf<String>()
+private val filteredMenuImage=mutableListOf<Int>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding=FragmentSearchBinding.inflate(inflater,container,false)
-        adapter=MenuAdapter(originalMenuFoodName as MutableList<String>,
-            originalMenuItemPrice as MutableList<String>, originalMenuImage as MutableList<Int>
-        )
+        adapter=MenuAdapter(filteredMenuFoodName,filteredMenuItemPrice,filteredMenuImage)
         binding.menuRecyclerView.layoutManager=LinearLayoutManager(requireContext())
         binding.menuRecyclerView.adapter=adapter
+
+        // Setup for search view
+        setupSearchView()
+
+        //Show All Menu Items
+        showAllMenu()
         return binding.root
     }
+    private fun showAllMenu(){
+        filteredMenuFoodName.clear()
+        filteredMenuItemPrice.clear()
+        filteredMenuImage.clear()
+        filteredMenuFoodName.addAll(originalMenuFoodName)
+        filteredMenuItemPrice.addAll(originalMenuItemPrice)
+        filteredMenuImage.addAll(originalMenuImage)
 
+        adapter.notifyDataSetChanged()
+    }
+    private fun setupSearchView() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String): Boolean {
+                filterMenuItems(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                filterMenuItems(newText)
+                return true
+            }
+        })
+    }
+
+    private fun filterMenuItems(query: String) {
+        filteredMenuFoodName.clear()
+        filteredMenuItemPrice.clear()
+        filteredMenuImage.clear()
+        originalMenuFoodName.forEachIndexed { index, foodName ->
+            if (foodName.contains(query, ignoreCase = true)) {
+                filteredMenuFoodName.add(foodName)
+                filteredMenuItemPrice.add(originalMenuItemPrice[index])
+                filteredMenuImage.add(originalMenuImage[index])
+            }
+        }
+        adapter.notifyDataSetChanged()
+    }
     companion object {
 
     }
