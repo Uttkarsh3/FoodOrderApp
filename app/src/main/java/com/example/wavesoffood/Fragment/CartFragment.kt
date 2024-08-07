0ea1a7078edb865fc02bd3507cba298d7d31ce6d
uttkarsh <uttkarshbisht3@gmail.com>
2024-08-07 16:14:05 +0530

    Start Here

--- app/src/main/java/com/example/wavesoffood/Fragment/CartFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragment/CartFragment.kt
@@ -1,14 +1,17 @@
 package com.example.wavesoffood.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wavesoffood.CongratsBottomSheet
 import com.example.wavesoffood.R
 import com.example.wavesoffood.adapter.CartAdapter
 import com.example.wavesoffood.databinding.FragmentCartBinding
+import com.example.wavesoffood.payOutActivity
 
 
 class CartFragment : Fragment() {
@@ -35,6 +38,12 @@ class CartFragment : Fragment() {
         val adapter= CartAdapter(ArrayList(cartFoodName),ArrayList(cartItemPrice),ArrayList(cartImage))
         binding.cartrecyclerview.layoutManager=LinearLayoutManager(requireContext())
         binding.cartrecyclerview.adapter=adapter
+
+        binding.proceedButton.setOnClickListener{
+            val intent= Intent(requireContext(),payOutActivity::class.java)
+            startActivity(intent)
+        }
+
         return binding.root
     }
 
